6fd5900cf17f11028f16c30c3bc72e0c1364deb7
parmarashwinvikrambhai <103418850+parmarashwinvikrambhai@users.noreply.github.com>
2023-02-23 17:42:52 +0530

    Add files via upload

---
+++ patterns/diamond.php
@@ -0,0 +1,29 @@
+<?php
+
+for($i=1;$i<=5;$i++)
+{   
+    for($k=1;$k<=5-$i;$k++)
+    {
+        echo "&nbsp;&nbsp";
+    }                                                                    
+     for($j=1;$j<=$i;$j++)
+     {
+        echo "*&nbsp;&nbsp";
+     }
+     echo"<br/>";
+}
+for($i=4;$i>=1;$i--)
+{   
+    for($k=1;$k<=5-$i;$k++)
+    {
+        echo "&nbsp;&nbsp";
+    }                                                                    
+     for($j=1;$j<=$i;$j++)
+     {
+        echo "*&nbsp;&nbsp";
+     }
+     echo"<br/>";
+}
+
+
+?>
---
+++ patterns/left.php
@@ -0,0 +1,54 @@
+<?php
+for($i=1;$i<=5;$i++)
+    {                                                                       
+         for($j=1;$j<=$i;$j++)
+         {
+            echo "*";
+         }
+         echo"<br><br>";
+    }
+
+
+
+
+    for($i=1;$i<=5;$i++)
+    {   
+        for($k=5;$k>=$i;$k--)
+        {
+            echo "&nbsp;&nbsp";
+        }                                                                    
+         for($j=1;$j<=$i;$j++)
+         {
+            echo "*";
+         }
+         echo"<br><br>";
+
+    }
+   echo "<br><br><br>";
+    for($i=5;$i>=1;$i--)
+    {                                                                       
+         for($j=1;$j<=$i;$j++)
+         {
+            echo "*";
+         }
+         echo"<br><br>";
+
+    }
+
+    echo "<br><br><br>";
+
+            //     *
+    for($i=5;$i>=1;$i--)
+    {   
+        for($k=5;$k>=$i;$k--)
+        {
+            echo "&nbsp;&nbsp";
+        }                                                                    
+         for($j=1;$j<=$i;$j++)
+         {
+            echo "*";
+         }
+         echo"<br><br>";
+
+    }
+    ?>
